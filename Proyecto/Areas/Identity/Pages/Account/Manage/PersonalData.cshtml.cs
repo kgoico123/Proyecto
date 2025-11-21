e551a2da0fc9e1072c8254b3cc9cce2eabf2a820
kevin yordin goicochea idrogo <kevin.goicochea.uer@gmail.com>
2025-11-21 18:56:01 -0500

    fix(identity): remove unused logger from PersonalDataModel

--- Proyecto/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ Proyecto/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -13,14 +13,11 @@ namespace Proyecto.Areas.Identity.Pages.Account.Manage
     public class PersonalDataModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly ILogger<PersonalDataModel> _logger;
 
         public PersonalDataModel(
-            UserManager<ApplicationUser> userManager,
-            ILogger<PersonalDataModel> logger)
+            UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
-            _logger = logger;
         }
 
         public async Task<IActionResult> OnGet()
