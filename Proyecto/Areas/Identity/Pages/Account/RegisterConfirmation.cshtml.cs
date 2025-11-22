8ba8e4095343478c7f03b45dfb46ec98d44e7cda
kevin yordin goicochea idrogo <kevin.goicochea.uer@gmail.com>
2025-11-22 12:46:10 -0500

    chore(identity): remove unused IEmailSender from RegisterConfirmationModel

--- Proyecto/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ Proyecto/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -19,12 +19,10 @@ namespace Proyecto.Areas.Identity.Pages.Account
     public class RegisterConfirmationModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IEmailSender _sender;
 
-        public RegisterConfirmationModel(UserManager<ApplicationUser> userManager, IEmailSender sender)
+        public RegisterConfirmationModel(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
-            _sender = sender;
         }
 
         /// <summary>
