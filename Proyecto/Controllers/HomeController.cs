bf9e048a78072ff7bd3566b6255594961c55f37e
kevin yordin goicochea idrogo <kevin.goicochea.uer@gmail.com>
2025-11-22 12:56:17 -0500

    chore(home): remove unused ILogger from HomeController

--- Proyecto/Controllers/HomeController.cs
+++ Proyecto/Controllers/HomeController.cs
@@ -7,12 +7,6 @@ namespace Proyecto.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
